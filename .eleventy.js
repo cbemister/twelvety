46501c1526f7d646e31587b15a89fc8c31b8b0bb
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2022-02-20 17:55:46 -0700

    set-up netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,8 @@ module.exports = function (config) {
   addTransforms(config);
   addShortcodes(config);
 
+  eleventyConfig.addPassthroughCopy("admin");
+
   // Extract and inline critical CSS in production
   // Documentation: https://github.com/gregives/eleventy-critical-css
   if (twelvety.env === "production") {
